9c9f6179ba92c7439366e42dda6999af0e7eea8c
Thomas Anesta <contact@thomasanesta.com>
2013-07-22 10:51:36 -0400

    some additions to keylogic

--- containers/KeyLogic.cs
+++ containers/KeyLogic.cs
@@ -13,6 +13,11 @@ namespace monoswitch.containers
         NOT, OR, AND, XOR, LEFT, RIGHT, BI
     }
 
+    public enum methods
+    {
+        ACTIVATE, DEACTIVATE
+    }
+
     public class KeyLogicRoot : KeyLogicNode
     {
 
@@ -259,20 +264,208 @@ namespace monoswitch.containers
 
         #endregion
 
-
-
-
     }
 
     public class KeyLogicDictionary : ITreeNode<KeyLogicDictionary>
     {
 
-        private TreeNode<KeyLogicDictionary> m_parent;
+        #region members_memberlike_properties
 
-        public TreeNode<KeyLogicDictionary> GetTreeNode()
-        {
-            return this.m_parent;
-        }
+            #region public
+
+            #endregion
+
+            #region internal
+
+            #endregion
+
+            #region protected
+
+                protected TreeNode<KeyLogicDictionary> m_parent;
+                protected Dictionary<Keys, bool> m_dict;
+
+            #endregion
+
+            #region private
+
+            #endregion
+
+        #endregion
+
+        #region properties
+
+            #region public
+
+                public bool isTrue
+                {
+                    get
+                    {
+                        bool rVal = true;
+                        foreach (Keys kVal in this.m_dict.Keys)
+                        {
+                            if (!this.m_dict[kVal])
+                            {
+                                rVal = false;
+                                break;
+                            }
+                        }
+                        return rVal;
+                    }
+                }
+
+                public bool isFalse
+                {
+                    get
+                    {
+                        bool rVal = true;
+                        foreach (Keys kVal in this.m_dict.Keys)
+                        {
+                            if (this.m_dict[kVal])
+                            {
+                                rVal = false;
+                                break;
+                            }
+                        }
+                        return rVal;
+                    }
+                }
+
+                public bool isIndeterminate
+                {
+                    get
+                    {
+                        return (this.isTrue && this.isFalse);
+                    }
+                }
+
+            #endregion
+
+            #region internal
+
+            #endregion
+
+            #region protected
+
+            #endregion
+
+            #region private
+
+            #endregion
+
+        #endregion
+
+        #region events
+
+            #region public
+
+            #endregion
+
+            #region internal
+
+            #endregion
+
+            #region protected
+
+            #endregion
+
+            #region private
+
+            #endregion
+
+        #endregion
+
+        #region methods
+
+            #region public
+
+                //constructor
+                public KeyLogicDictionary()
+                {
+                    this.m_parent = null;
+                    this.m_dict = new Dictionary<Keys, bool>();
+                }
+
+                public TreeNode<KeyLogicDictionary> GetTreeNode()
+                {
+                    return this.m_parent;
+                }
+
+                public void add(Keys kVal)
+                {
+                    if (!this.m_dict.ContainsKey(kVal))
+                    {
+                        this.m_dict[kVal] = false;
+                    }
+                }
+
+                public void add(List<Keys> kList)
+                {
+                    foreach (Keys kVal in kList)
+                    {
+                        this.add(kVal);
+                    }
+                }
+
+                //control methods
+                public void deactivate(List<Keys> dVal)
+                {
+                    if (dVal.Count == 0)
+                    {
+                        foreach (Keys kv in this.m_dict.Keys)
+                        {
+                            this.m_dict[kv] = false;
+                        }
+                    }
+                    foreach (Keys kv in dVal)
+                    {
+                        if (this.m_dict.ContainsKey(kv))
+                        {
+                            this.m_dict[kv] = false;
+                        }
+                    }
+                }
+
+                public void activate(List<Keys> aVal)
+                {
+                    if (aVal.Count == 0)
+                    {
+                        foreach (Keys kv in this.m_dict.Keys)
+                        {
+                            this.m_dict[kv] = true;
+                        }
+                    }
+                    foreach (Keys kv in aVal)
+                    {
+                        if (this.m_dict.ContainsKey(kv))
+                        {
+                            this.m_dict[kv] = true;
+                        }
+                    }
+                }
+
+                
+
+            #endregion
+
+            #region internal
+
+            #endregion
+
+            #region protected
+
+            #endregion
+
+            #region private
+
+            #endregion
+
+        #endregion
+
+        
+
+        
+
+        
 
     }
 
